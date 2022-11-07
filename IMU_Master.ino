455e77ffe56c8b57aebd3fd513276ab561565c17
hihi2033 <61200758+hihi2033@users.noreply.github.com>
2022-11-07 22:34:06 +0800

    Update IMU_Master.ino

--- IMU_Master.ino
+++ IMU_Master.ino
@@ -1,38 +1,18 @@
-/* Controlling ESP32 Mecanum Wheel car by PS4 Controller
- * created by: Winston Yeung
- * revision date: 16 Oct 2022
-*/
-//Mecanum Wheel car and slave
-
 #include <esp_now.h>
 #include <WiFi.h>
+#include "Wire.h"
 
 #define CHANNEL 1
+esp_now_peer_info_t slave;
 
-#define FORWARD 1
-#define BACKWARD 2
-#define LEFT 3
-#define RIGHT 4
-#define FORWARD_LEFT 5
-#define FORWARD_RIGHT 6
-#define BACKWARD_LEFT 7
-#define BACKWARD_RIGHT 8
-#define ROTATE_LEFT 9
-#define ROTATE_RIGHT 10
-#define STOP 0
+uint8_t broadcastAddress[] = {0xE8,0x68,0xE7,0x22,0xAD,0xEC}; // hard code broadcast address for receiver
 
-#define FRONT_RIGHT_MOTOR 1
-#define FRONT_LEFT_MOTOR 2
-#define BACK_LEFT_MOTOR 3
-#define BACK_RIGHT_MOTOR 4
-
-#define MAX_MOTOR_SPEED 250 // about 50% duty cycle
-
-//slave init
+//uint8_t data = 0;
 float theta_gyro;
 float phi_gyro;
 float psi_gyro;
-
+// Structure example to send data
+// Must match the receiver structure
 typedef struct struct_message {
   //uint8_t data;
   float theta_gyro;
@@ -43,356 +23,173 @@ typedef struct struct_message {
 // Create a struct_message called myData
 struct_message myData;
 
-// callback when data is recv from Master
-void OnDataRecv(const uint8_t *mac_addr, const uint8_t *incomingData, int data_len) {
-  memcpy(&myData, incomingData, sizeof(myData));
-  Serial.println("Received  :");
-  //Serial.println(myData.data);
-  //newData = myData.data * 10;
-  Serial.print("theta_gyro: ");
-  Serial.println(myData.theta_gyro);
-  Serial.print("phi_gyro: ");
-  Serial.println(myData.phi_gyro);
-  Serial.print("psi_gyro: ");
-  Serial.println(myData.psi_gyro);
-  
-  theta_gyro = myData.theta_gyro;
-  phi_gyro = myData.phi_gyro;
-  psi_gyro = myData.psi_gyro;
-  
-  if (phi_gyro < -20 && theta_gyro < -20) {Serial.println("FORWARD_LEFT");moveCar(FORWARD_LEFT);}
-  else if (phi_gyro < -20 && theta_gyro > 20) {Serial.println("FORWARD_RIGHT");moveCar(FORWARD_RIGHT);}
-  else if (phi_gyro > 20 && theta_gyro < -20) {Serial.println("BACKWARD_LEFT");moveCar(BACKWARD_LEFT);}
-  else if (phi_gyro > 20 && theta_gyro > 20) {Serial.println("BACKWARD_RIGHT");moveCar(BACKWARD_RIGHT);}
-  else if (phi_gyro < -20) {Serial.println("FORWARD");moveCar(FORWARD);}
-  else if (phi_gyro > 20) {Serial.println("BACKWARD");moveCar(BACKWARD);}
-  else if (theta_gyro < -20) {Serial.println("LEFT");moveCar(LEFT);}
-  else if (theta_gyro > 20) {Serial.println("RIGHT");moveCar(RIGHT);}
-  else if (psi_gyro > 20) {Serial.println("ROTATE_LEFT");moveCar(ROTATE_LEFT);}
-  else if (psi_gyro < -20) {Serial.println("ROTATE_RIGHT");moveCar(ROTATE_RIGHT);}
-  else {Serial.println("STOP");moveCar(STOP);}
-  Serial.println();
-  //delay(100);
+
+//IMU init code
+const int IMU_ADDR = 0x68; //ADO=5V -> 0x69
+
+// Declare variables.
+// NOTE: theta = pitch, phi = roll, psi = yaw
+int16_t accel_X, accel_Y, accel_Z, temp_raw, gyro_X, gyro_Y, gyro_Z;
+float aX, aY, aZ, theta_acc, phi_acc; //accel values and theta and phi calculated from accelerometer
+float theta_acc_previous = 0, theta_acc_LP, phi_acc_previous = 0, phi_acc_LP;
+float gX, gY, gZ;// ,theta_gyro=0, phi_gyro=0, psi_gyro;
+unsigned long previousTime, elapseTime;
+float dt;
+
+
+// callback when data is sent from Master to Slave
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+//  Serial.print("\r\nLast Packet Send Status:\t");
+//  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 }
 
-unsigned long lastTimeStamp = 0;
+void setup() {
+  Serial.begin(115200); // Init Serial Monitor
+  WiFi.mode(WIFI_STA); // Set device as a Wi-Fi Station
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK){
+ //   Serial.println("Error init");
+    return;
+  }
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  esp_now_register_send_cb(OnDataSent);
+// Register peer
+  memcpy(slave.peer_addr, broadcastAddress, 6);
+  slave.channel = CHANNEL; // pick a channel
+  slave.encrypt = false; // no encryption
+  // Add peer
+  if (esp_now_add_peer(&slave) !=ESP_OK){
+  //  Serial.println("Failed to add peer");
+    return;
+  }
 
-//FRONT RIGHT MOTOR
-int enableFrontRightMotor = 14; 
-int FrontRightMotorPin1 = 26;
-int FrontRightMotorPin2 = 27;
-//BACK RIGHT MOTOR
-int enableBackRightMotor=22; 
-int BackRightMotorPin1=16;
-int BackRightMotorPin2=17;
-//FRONT LEFT MOTOR
-int enableFrontLeftMotor = 32;
-int FrontLeftMotorPin1 = 33;
-int FrontLeftMotorPin2 = 25;
-//BACK LEFT MOTOR
-int enableBackLeftMotor = 23;
-int BackLeftMotorPin1 = 18;
-int BackLeftMotorPin2 = 19;
+  // IMU setup
+  Wire.begin();
+  Wire.beginTransmission(IMU_ADDR); //communicate with IMU
+  Wire.write(0x6B); //PWR_MGMT_1 register
+  Wire.write(0x0);
+  Wire.endTransmission(true);
 
-const int PWMFreq = 1000; /* 1 KHz */
-const int PWMResolution = 8; // 8-bit
-const int PWMSpeedChannel_1 = 1; // 0-15
-const int PWMSpeedChannel_2 = 2; // 0-15
-const int PWMSpeedChannel_3 = 3; // 0-15
-const int PWMSpeedChannel_4 = 4; // 0-15
+  Wire.beginTransmission(IMU_ADDR); //communicate with IMU
+  Wire.write(0x1C); //ACCEL_CONFIG register
+  Wire.write(0b00000000); //Bit3 & Bit 4 -> 0,0 -> +/-2g
+  Wire.endTransmission(true);
 
+  Wire.beginTransmission(IMU_ADDR); //communicate with IMU
+  Wire.write(0x1B); //GYRO_CONFIG register
+  Wire.write(0b00000000); //Bit3 & Bit 4 -> 0,0 -> +/-250deg/s
+  Wire.endTransmission(true);
 
-/*
-void notify()
-{
-  int theta_gyro = map( theta_gyro, -127, 127, -254, 254);
-  int phi_gyro = map( phi_gyro, -127, 127, -254, 254);
-  int psi_gyro = map( psi_gyro, -127, 127, -254, 254);
-  theta_gyro = myData.theta_gyro; // roll  stop =[-15,10],  move left < -15,    right > 10 
-  phi_gyro = myData.phi_gyro;     // pitch stop = [-25,15], move forward <-25,  backward > 15
-  psi_gyro = myData.psi_gyro;     // yaw  stop = [-15,20],  turn right < -15,   turn left > 20
+  previousTime = millis();
+}
 
-  if (phi_gyro < -20 && theta_gyro < -20) {moveCar(FORWARD_LEFT);}
-  else if (phi_gyro < -20 && theta_gyro > 20) {moveCar(FORWARD_RIGHT);}
-  else if (phi_gyro > 20 && theta_gyro < -20) {moveCar(BACKWARD_LEFT);}
-  else if (phi_gyro > 20 && theta_gyro > 20) {moveCar(BACKWARD_RIGHT);}
-  else if (phi_gyro < -20) {moveCar(FORWARD);}
-  else if (phi_gyro > 20) {moveCar(BACKWARD);}
-  else if (theta_gyro < -20) {moveCar(LEFT);}
-  else if (theta_gyro > 20) {moveCar(RIGHT);}
-  else if (psi_gyro > 20) {moveCar(ROTATE_LEFT);}
-  else if (psi_gyro < -20) {moveCar(ROTATE_RIGHT);}
-  else {moveCar(STOP);}
+void loop() {
+  myData.theta_gyro = theta_gyro;
+  myData.phi_gyro = phi_gyro;
+  myData.psi_gyro = psi_gyro;
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
+  if (result == ESP_OK) {
+     //Serial.println("Sent with success");
+  }
+  else {
+  //  Serial.println("Error sending the data");
+  }
+  //data++;
 
-// Print data for debugging purpose only
-if (millis() - lastTimeStamp > 50)
-  {
-  Serial.print(phi_gyro);
-  Serial.print(',');
+  Wire.beginTransmission(IMU_ADDR);
+  Wire.write(0x3B); // start from ACCEL_XOUT_H
+  Wire.endTransmission(false);
+  Wire.requestFrom(IMU_ADDR, 14, true); //request a total of 14 bytes
+
+  // read registers from IMU
+  accel_X = Wire.read()<<8 | Wire.read(); //read registers 0x3B & 0x3C
+  accel_Y = Wire.read()<<8 | Wire.read(); //read registers 0x3D & 0x3E
+  accel_Z = Wire.read()<<8 | Wire.read(); //read registers 0x3F & 0x40
+  temp_raw = Wire.read()<<8 | Wire.read(); //read registers 0x41 & 0x42
+  gyro_X = Wire.read()<<8 | Wire.read(); //read registers 0x43 & 0x44
+  gyro_Y = Wire.read()<<8 | Wire.read(); //read registers 0x45 & 0x46
+  gyro_Z = Wire.read()<<8 | Wire.read(); //read registers 0x47 & 0x48
+  
+  Wire.endTransmission(true);
+
+//  Serial.print("aX_raw: "); Serial.print(accel_X);Serial.print(", ");
+//  Serial.print("aY_raw: "); Serial.print(accel_Y);Serial.print(", ");
+//  Serial.print("aZ_raw: "); Serial.print(accel_Z);Serial.print(", ");
+//  Serial.print("gX_raw: "); Serial.print(gyro_X);Serial.print(", ");
+//  Serial.print("gY_raw: "); Serial.print(gyro_Y);Serial.print(", ");
+//  Serial.print("gZ_raw: "); Serial.print(gyro_Z);Serial.print(", ");
+//  Serial.print("Temp_raw: "); Serial.print(temp_raw);
+//  Serial.print("aX: "); Serial.print(accel_X/16384.0);Serial.print(", ");
+//  Serial.print("aY: "); Serial.print(accel_Y/16384.0);Serial.print(", ");
+//  Serial.print("aZ: "); Serial.print(accel_Z/16384.0);
+//  Serial.println();
+  
+  aX = accel_X/16384.0; // convert the raw data to g value. 1g = 9.8 m/s2
+  aY = accel_Y/16384.0;
+  aZ = accel_Z/16384.0;
+
+ //--------------Getting Roll(phi) and Pitch(theta) from accelerometer readings only-------------------// 
+  theta_acc = atan2(aX, aZ)*(180/PI); // approximation of theta (pitch). use either this or the eauation right below.
+  // theta_acc = atan2(aX, sqrt(pow(aY,2)+pow(aZ,2)))*(180/PI); // another approximation on theta (pitch). Either use this or the equation right above.
+  phi_acc = atan2(aY, aZ)*(180/PI); // approximation of phi (roll). use either this or the eauation right below.
+  // phi_acc = atan2(aY, sqrt(pow(aX,2)+pow(aZ,2)))*(180/PI);// another approximation on phi (roll). Either use this or the equation right above.
+
+  // simple Low Pass Filter (LPF) added for theta(pitch) and phi(roll)
+  theta_acc_LP = 0.7*theta_acc_previous + 0.3*theta_acc; // adjust the ratio of preivous theta value and the newly measured value
+  phi_acc_LP = 0.7*phi_acc_previous + 0.3*phi_acc;
+
+  theta_acc_previous = theta_acc_LP;
+  phi_acc_previous = phi_acc_LP;
+//-------------------------------------------------------------------------------------//
+
+//--------------Getting Roll(phi) and Pitch(theta) from gyroscope readings only-------------------//
+  gY = gyro_Y/131.0 -0.138; //+1.93
+  gX = gyro_X/131.0 +0.45; //-1.5 convert the raw data to rad/s. "+2.2" is the gyro error
+  gZ = gyro_Z/131.0 -0.6;   //-0.9375
+
+  elapseTime = millis() - previousTime;
+  dt = elapseTime / 1000.0;
+  previousTime = millis();
+
+ theta_gyro = theta_gyro + gY * dt; // pitch angle from gyro
+  phi_gyro = phi_gyro + gX * dt; // roll angle from gyro
+  psi_gyro = psi_gyro + gZ * dt ; // yaw angle from gyro
+//------------------------------------------------------------------------------------------------//
+if (theta_gyro>40){
+  theta_gyro=40;
+}
+else if (theta_gyro<-40){
+  theta_gyro=-40;
+}
+if (phi_gyro>40){
+  phi_gyro=40;
+}
+else if (phi_gyro<-40){
+  phi_gyro=-40;
+}
+if ( psi_gyro>40){
+   psi_gyro=40;
+}
+else if ( psi_gyro<-40){
+   psi_gyro=-40;
+}
+/*//  Print all data to serial plot 
+  Serial.print(theta_acc);
+  Serial.print(",");
+  Serial.print(phi_acc);
+  Serial.print(",");
+  Serial.print(theta_acc_LP); // theta value from accel with LPF
+  Serial.print(",");
+  Serial.print(phi_acc_LP);
+//
+  Serial.print(",");*/
   Serial.print(theta_gyro);
-  Serial.print(',');
+  Serial.print(",");
+  Serial.print(phi_gyro);
+  Serial.print(",");
   Serial.print(psi_gyro);
   Serial.println();
-  lastTimeStamp = millis();
-  }
-}
-*/
-void onConnect()
-{
-  Serial.println("Connected!");
-}
-
-void onDisConnect()
-{
-  //rotateMotor(0, 0);
-  rotateMotor(BACK_RIGHT_MOTOR, 0);
-  rotateMotor(BACK_LEFT_MOTOR, 0);
-  rotateMotor(FRONT_RIGHT_MOTOR, 0);
-  rotateMotor(FRONT_LEFT_MOTOR, 0);
-}
-
-void moveCar(int inputValue)
-{
-  switch(inputValue)
-  {
-    case FORWARD:
-      rotateMotor(FRONT_RIGHT_MOTOR, MAX_MOTOR_SPEED);
-      rotateMotor(BACK_RIGHT_MOTOR, MAX_MOTOR_SPEED);
-      rotateMotor(FRONT_LEFT_MOTOR, MAX_MOTOR_SPEED);
-      rotateMotor(BACK_LEFT_MOTOR, MAX_MOTOR_SPEED);                  
-      break;
   
-    case BACKWARD:
-      rotateMotor(FRONT_RIGHT_MOTOR, -MAX_MOTOR_SPEED);
-      rotateMotor(BACK_RIGHT_MOTOR, -MAX_MOTOR_SPEED);
-      rotateMotor(FRONT_LEFT_MOTOR, -MAX_MOTOR_SPEED);
-      rotateMotor(BACK_LEFT_MOTOR, -MAX_MOTOR_SPEED);   
-      break;
-  
-    case RIGHT:
-      rotateMotor(FRONT_RIGHT_MOTOR, -MAX_MOTOR_SPEED);
-      rotateMotor(BACK_RIGHT_MOTOR, MAX_MOTOR_SPEED);
-      rotateMotor(FRONT_LEFT_MOTOR, MAX_MOTOR_SPEED);
-      rotateMotor(BACK_LEFT_MOTOR, -MAX_MOTOR_SPEED);   
-      break;
-  
-    case LEFT:
-      rotateMotor(FRONT_RIGHT_MOTOR, MAX_MOTOR_SPEED);
-      rotateMotor(BACK_RIGHT_MOTOR, -MAX_MOTOR_SPEED);
-      rotateMotor(FRONT_LEFT_MOTOR, -MAX_MOTOR_SPEED);
-      rotateMotor(BACK_LEFT_MOTOR, MAX_MOTOR_SPEED);  
-      break;
-  
-    case FORWARD_LEFT:
-      rotateMotor(FRONT_RIGHT_MOTOR, MAX_MOTOR_SPEED);
-      rotateMotor(BACK_RIGHT_MOTOR, STOP);
-      rotateMotor(FRONT_LEFT_MOTOR, STOP);
-      rotateMotor(BACK_LEFT_MOTOR, MAX_MOTOR_SPEED);  
-      break;
-  
-    case FORWARD_RIGHT:
-      rotateMotor(FRONT_RIGHT_MOTOR, STOP);
-      rotateMotor(BACK_RIGHT_MOTOR, MAX_MOTOR_SPEED);
-      rotateMotor(FRONT_LEFT_MOTOR, MAX_MOTOR_SPEED);
-      rotateMotor(BACK_LEFT_MOTOR, STOP);  
-      break;
-  
-    case BACKWARD_LEFT:
-      rotateMotor(FRONT_RIGHT_MOTOR, STOP);
-      rotateMotor(BACK_RIGHT_MOTOR, -MAX_MOTOR_SPEED);
-      rotateMotor(FRONT_LEFT_MOTOR, -MAX_MOTOR_SPEED);
-      rotateMotor(BACK_LEFT_MOTOR, STOP);   
-      break;
-  
-    case BACKWARD_RIGHT:
-      rotateMotor(FRONT_RIGHT_MOTOR, -MAX_MOTOR_SPEED);
-      rotateMotor(BACK_RIGHT_MOTOR, STOP);
-      rotateMotor(FRONT_LEFT_MOTOR, STOP);
-      rotateMotor(BACK_LEFT_MOTOR, -MAX_MOTOR_SPEED);   
-      break;
-  
-    case ROTATE_LEFT:
-      rotateMotor(FRONT_RIGHT_MOTOR, MAX_MOTOR_SPEED);
-      rotateMotor(BACK_RIGHT_MOTOR, MAX_MOTOR_SPEED);
-      rotateMotor(FRONT_LEFT_MOTOR, -MAX_MOTOR_SPEED);
-      rotateMotor(BACK_LEFT_MOTOR, -MAX_MOTOR_SPEED);  
-      break;
-  
-    case ROTATE_RIGHT:
-      rotateMotor(FRONT_RIGHT_MOTOR, -MAX_MOTOR_SPEED);
-      rotateMotor(BACK_RIGHT_MOTOR, -MAX_MOTOR_SPEED);
-      rotateMotor(FRONT_LEFT_MOTOR, MAX_MOTOR_SPEED);
-      rotateMotor(BACK_LEFT_MOTOR, MAX_MOTOR_SPEED);   
-      break;
-  
-    case STOP:
-      rotateMotor(FRONT_RIGHT_MOTOR, STOP);
-      rotateMotor(BACK_RIGHT_MOTOR, STOP);
-      rotateMotor(FRONT_LEFT_MOTOR, STOP);
-      rotateMotor(BACK_LEFT_MOTOR, STOP);    
-      break;
-  
-    default:
-      rotateMotor(FRONT_RIGHT_MOTOR, STOP);
-      rotateMotor(BACK_RIGHT_MOTOR, STOP);
-      rotateMotor(FRONT_LEFT_MOTOR, STOP);
-      rotateMotor(BACK_LEFT_MOTOR, STOP);    
-      break;
-  }
-}
-
-void rotateMotor(int motorNumber, int motorSpeed)
-{
-  /*if (motorSpeed < 0)
-  {
-    digitalWrite(FrontRightMotorPin1,LOW);
-    digitalWrite(FrontRightMotorPin2,HIGH);
-    digitalWrite(FrontLeftMotorPin1,LOW);
-    digitalWrite(FrontLeftMotorPin2,HIGH);
-    digitalWrite(BackRightMotorPin1,LOW);
-    digitalWrite(BackRightMotorPin2,HIGH);     
-    digitalWrite(BackLeftMotorPin1,LOW);
-    digitalWrite(BackLeftMotorPin2,HIGH);
-  }
-  else if (motorSpeed > 0)
-  {
-    digitalWrite(FrontRightMotorPin1,HIGH);
-    digitalWrite(FrontRightMotorPin2,LOW);  
-    digitalWrite(FrontLeftMotorPin1,HIGH);
-    digitalWrite(FrontLeftMotorPin2,LOW);
-    digitalWrite(BackRightMotorPin1,HIGH);
-    digitalWrite(BackRightMotorPin2,LOW);     
-    digitalWrite(BackLeftMotorPin1,HIGH);
-    digitalWrite(BackLeftMotorPin2,LOW);
-  }
-  else
-  {
-    digitalWrite(FrontRightMotorPin1,LOW);
-    digitalWrite(FrontRightMotorPin2,LOW);     
-    digitalWrite(FrontLeftMotorPin1,LOW);
-    digitalWrite(FrontLeftMotorPin2,LOW);
-    digitalWrite(BackRightMotorPin1,LOW);
-    digitalWrite(BackRightMotorPin2,LOW);     
-    digitalWrite(BackLeftMotorPin1,LOW);
-    digitalWrite(BackLeftMotorPin2,LOW);   
-  }*/
-  if (motorSpeed < 0 && motorNumber == 1)
-    {
-    digitalWrite(BackRightMotorPin1,LOW);
-    digitalWrite(BackRightMotorPin2,HIGH);  
-    }
-    else if (motorSpeed < 0 && motorNumber == 2)  
-    { 
-    digitalWrite(BackLeftMotorPin1,LOW);
-    digitalWrite(BackLeftMotorPin2,HIGH);
-    }
-    else if (motorSpeed < 0 && motorNumber == 3)
-    {
-    digitalWrite(FrontRightMotorPin1,LOW);
-    digitalWrite(FrontRightMotorPin2,HIGH);  
-    }
-    else if (motorSpeed < 0 && motorNumber == 4)  
-    { 
-    digitalWrite(FrontLeftMotorPin1,LOW);
-    digitalWrite(FrontLeftMotorPin2,HIGH);
-    } 
-
-    else if (motorSpeed > 0 && motorNumber == 1)
-    {
-    digitalWrite(BackRightMotorPin1,HIGH);
-    digitalWrite(BackRightMotorPin2,LOW);
-    }
-    else if (motorSpeed > 0 && motorNumber == 2)
-    {
-    digitalWrite(BackLeftMotorPin1,HIGH);
-    digitalWrite(BackLeftMotorPin2,LOW);
-    }
-    else if (motorSpeed > 0 && motorNumber == 3)
-    {
-    digitalWrite(FrontRightMotorPin1,HIGH);
-    digitalWrite(FrontRightMotorPin2,LOW);  
-    }
-    else if (motorSpeed > 0 && motorNumber == 4)  
-    { 
-    digitalWrite(FrontLeftMotorPin1,HIGH);
-    digitalWrite(FrontLeftMotorPin2,LOW);
-    }
-
-    else if (motorSpeed == 0 && motorNumber == 1)
-    {
-    digitalWrite(FrontRightMotorPin1,LOW);
-    digitalWrite(FrontRightMotorPin2,LOW);     
-    }
-    else if (motorSpeed == 0 && motorNumber == 2)
-    {
-    digitalWrite(FrontLeftMotorPin1,LOW);
-    digitalWrite(FrontLeftMotorPin2,LOW);
-    }
-    else if (motorSpeed == 0 && motorNumber == 3)
-    {
-    digitalWrite(BackRightMotorPin1,LOW);
-    digitalWrite(BackRightMotorPin2,LOW);  
-    }
-    else if (motorSpeed == 0 && motorNumber == 4)
-    {   
-    digitalWrite(BackLeftMotorPin1,LOW);
-    digitalWrite(BackLeftMotorPin2,LOW);   
-    }
-    ledcWrite(PWMSpeedChannel_1, abs(motorSpeed));
-    ledcWrite(PWMSpeedChannel_2, abs(motorSpeed));
-    ledcWrite(PWMSpeedChannel_3, abs(motorSpeed));
-    ledcWrite(PWMSpeedChannel_4, abs(motorSpeed));
-}
-
-void setUpPinModes()
-{
-  pinMode(enableFrontRightMotor,OUTPUT);
-  pinMode(FrontRightMotorPin1,OUTPUT);
-  pinMode(FrontRightMotorPin2,OUTPUT);
-  
-  pinMode(enableFrontLeftMotor,OUTPUT);
-  pinMode(FrontLeftMotorPin1,OUTPUT);
-  pinMode(FrontLeftMotorPin2,OUTPUT);
-
-  pinMode(enableBackRightMotor,OUTPUT);
-  pinMode(BackRightMotorPin1,OUTPUT);
-  pinMode(BackRightMotorPin2,OUTPUT);
-  
-  pinMode(enableBackLeftMotor,OUTPUT);
-  pinMode(BackLeftMotorPin1,OUTPUT);
-  pinMode(BackLeftMotorPin2,OUTPUT);
-
-  //Set up PWM for motor speed
-  /*ledcSetup(PWMSpeedChannel, PWMFreq, PWMResolution);
-  ledcAttachPin(enableFrontRightMotor, PWMSpeedChannel);
-  ledcAttachPin(enableFrontLeftMotor, PWMSpeedChannel); 
-  ledcAttachPin(enableBackRightMotor, PWMSpeedChannel);
-  ledcAttachPin(enableBackLeftMotor, PWMSpeedChannel);
-  ledcWrite(PWMSpeedChannel, MAX_MOTOR_SPEED);
-
-  rotateMotor(0, 0);*/
-  ledcSetup(PWMSpeedChannel_1, PWMFreq, PWMResolution);
-  ledcSetup(PWMSpeedChannel_2, PWMFreq, PWMResolution);
-  ledcSetup(PWMSpeedChannel_3, PWMFreq, PWMResolution);
-  ledcSetup(PWMSpeedChannel_4, PWMFreq, PWMResolution);
-  ledcAttachPin(enableBackRightMotor, PWMSpeedChannel_1);
-  ledcAttachPin(enableBackLeftMotor, PWMSpeedChannel_2);
-  ledcAttachPin(enableFrontRightMotor, PWMSpeedChannel_3);
-  ledcAttachPin(enableFrontLeftMotor, PWMSpeedChannel_4); 
-    
-  rotateMotor(BACK_RIGHT_MOTOR, 0);
-  rotateMotor(BACK_LEFT_MOTOR, 0);
-  rotateMotor(FRONT_RIGHT_MOTOR, 0);
-  rotateMotor(FRONT_LEFT_MOTOR, 0);
-}
-
-
-void setup()
-{
-
-}
-
-void loop()
-{
-  
+  //
 }
